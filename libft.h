fecc19eb4c9be545d5e50aab00745a73484f2f30
Tom <tmasur@mail.de>
2022-01-31 00:08:32 +0100

    Update libft.h

--- libft.h
+++ libft.h
@@ -10,11 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef LIBFT_H
-# define LIBFT_H
+#ifndef LIBFT_H
+#define LIBFT_H
 
-# include <stdlib.h>
-# include <unistd.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
